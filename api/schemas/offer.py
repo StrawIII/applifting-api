819e5dc0c3998030be62d6263b04803971f25ae2
Roman Seiner <romisseiner@gmail.com>
2024-11-19 17:45:28 +0100

    Enable orm_mode

--- api/schemas/offer.py
+++ api/schemas/offer.py
@@ -1,9 +1,11 @@
 from uuid import UUID
 
-from pydantic import BaseModel, field_serializer
+from pydantic import BaseModel, ConfigDict, field_serializer
 
 
 class Offer(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
     id: UUID
     price: int
     items_in_stock: int
