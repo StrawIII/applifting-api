c2481606fe135c886393657f12b07efd95d74f2e
Roman Seiner <romisseiner@gmail.com>
2024-11-21 12:06:26 +0100

    Add "test_product_id" fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 from typing import Any, Generator
+from uuid import UUID, uuid4
 
 import pytest
 from fastapi.testclient import TestClient
@@ -10,3 +11,19 @@ from api.main import app
 def test_client() -> Generator[TestClient, Any, None]:
     with TestClient(app) as client:
         yield client
+
+
+@pytest.fixture(scope="function")
+def test_product_id(test_client) -> Generator[UUID, Any, None]:
+    product_id = uuid4()
+    test_client.post(
+        "/api/v1/products",
+        json={
+            "id": str(product_id),
+            "name": "Tablet",
+            "description": "12.9, 10000 mAh",
+        },
+    )
+    yield product_id
+    # if not testing in a container
+    test_client.delete(f"/api/v1/products/{product_id}")
